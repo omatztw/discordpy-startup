5b63f1ab9a60bdd71516b22e4cab0d95170adc9a
おまつ先生 <33661309+omatztw@users.noreply.github.com>
2020-03-02 19:48:58 +0900

    Test

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,19 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
+from datetime import datetime, timedelta
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.event
+async def on_voice_state_update(member, before, after): 
+    if member.guild.id == 監視するサーバーid and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(通知させたいテキストチャンネルid)
+        if before.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
+        elif after.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            await alert_channel.send(msg)
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
+client.run(token)
