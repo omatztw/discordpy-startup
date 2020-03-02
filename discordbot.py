ccbd4bc5f1db2796fbeee43c6fc29349fa25905b
おまつ先生 <33661309+omatztw@users.noreply.github.com>
2020-03-02 20:21:40 +0900

    Update

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,5 @@
-import os
 import discord
+import os
 from datetime import datetime, timedelta
 
 client = discord.Client()
@@ -10,7 +10,7 @@ channel_id = os.environ['CHANNEL_ID']
 async def on_voice_state_update(member, before, after): 
     if before.channel != after.channel:
         now = datetime.utcnow() + timedelta(hours=9)
-        alert_channel = client.get_channel(channel_id)
+        alert_channel = client.get_channel(int(channel_id))
         if before.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
