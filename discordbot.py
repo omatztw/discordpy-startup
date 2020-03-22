b947aedd07dbf23bc5f42fd889ec9d477c4ad14a
おまつ先生 <33661309+omatztw@users.noreply.github.com>
2020-03-22 13:57:46 +0900

    Fix

--- discordbot.py
+++ discordbot.py
@@ -107,7 +107,7 @@ async def on_voice_state_update(member, before, after):
     if before.channel != after.channel:
         now = datetime.utcnow() + timedelta(hours=9)
         if before.channel is None:
-            mode = channel['mode']
+            mode = channel.mode
             if mode == 'all' or len(list(filter(lambda m: not m.bot, after.channel.members))) == 1:
                 msg = f'{now:%m/%d-%H:%M} に[{member.name}]さんがチャンネル[{after.channel.name}]で通話を始めました。'
                 await alert_channel.send(msg)
