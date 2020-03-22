ec68307243d3747c920e3cc8d6c17ef92f9bf24e
おまつ先生 <33661309+omatztw@users.noreply.github.com>
2020-03-22 17:08:06 +0900

    Add mention trigger

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,7 @@ def get_connection():
 def get_value_by_server_id(server_id):
     with get_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT voice_notification_channel as ch_id, mode as mode FROM server_info WHERE server_id='%s'" % (server_id,))
+            cur.execute("SELECT voice_notification_channel as ch_id, mode as mode, mention_everyone as mention_everyone FROM server_info WHERE server_id='%s'" % (server_id,))
             result = cur.fetchone()
             if result == None:
                 return None
@@ -26,7 +26,7 @@ def get_value_by_server_id(server_id):
 def get_all_data():
     with get_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT server_id as server_id, voice_notification_channel as ch_id, mode as mode FROM server_info")
+            cur.execute("SELECT server_id as server_id, voice_notification_channel as ch_id, mode as mode, mention_everyone as mention_everyone FROM server_info")
             result = cur.fetchall()
             return result
 
@@ -42,6 +42,12 @@ def change_mode(server_id, mode):
             cur.execute("UPDATE server_info SET mode = '%s' WHERE server_id = '%s'" % (mode, server_id))
         conn.commit()
 
+def update_mention_everyone(server_id, mention_everyone):
+    with get_connection() as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute("UPDATE server_info SET mention_everyone = '%s' WHERE server_id = '%s'" % (mention_everyone, server_id))
+        conn.commit()
+
 def get_channel_info_or_default(guild):
     channel = get_value_by_server_id(guild.id)
     if channel == None:
@@ -49,22 +55,25 @@ def get_channel_info_or_default(guild):
         if guild.system_channel:
             channel_id = guild.system_channel.id
         upsert_channel_id(guild.id, channel_id)
-        return channel_id, 'first'
-    return channel["ch_id"], channel["mode"]
+        return channel_id, 'first', False
+    return channel["ch_id"], channel["mode"], channel["mention_everyone"]
+
+def str2bool(s):
+     return s.lower() in ["true", "t", "yes", "1"]
 
 @client.event
 async def on_ready():
     global data_mem
     all_data = get_all_data()
     if all_data != None:
-        for key,ch_id, mode in all_data:
-            data_mem[key] = Server(key, ch_id, Mode.value_of(mode))
+        for key,ch_id, mode, mention_everyone in all_data:
+            data_mem[key] = Server(key, ch_id, Mode.value_of(mode), mention_everyone)
 
 @client.event
 async def on_guild_join(guild):
     global data_mem
-    ch_id, mode = get_channel_info_or_default(guild)
-    data_mem[str(guild.id)] = Server(guild.id, ch_id, Mode.value_of(mode))
+    ch_id, mode, mention_everyone = get_channel_info_or_default(guild)
+    data_mem[str(guild.id)] = Server(guild.id, ch_id, Mode.value_of(mode), mention_everyone)
 
 @client.command()
 async def oma(ctx, *arg):
@@ -85,6 +94,14 @@ async def oma(ctx, *arg):
         data_mem[str(ctx.message.guild.id)].mode = mode
         await ctx.send("通知モードを[%s]に変更しました。" % mode.value)
     
+    if arg[0] == 'mention':
+        if len(arg) != 2 or (arg[1] not in ['yes', 'no']):
+            await ctx.send("Usage: `!oma mention %s`" %  ['yes', 'no'])
+            return
+        
+        update_mention_everyone(ctx.message.guild.id, str2bool(arg[1]))
+        data_mem[str(ctx.message.guild.id)].mention_everyone = str2bool(arg[1])
+        await ctx.send("全員へのメンションを[%s]に変更しました。" % arg[1])
 
 @client.event
 async def on_voice_state_update(member, before, after):
@@ -95,17 +112,21 @@ async def on_voice_state_update(member, before, after):
     guild_id = str(member.guild.id)
     channel = data_mem.get(guild_id)
     if channel == None:
-        channel_id, mode = get_channel_info_or_default(member.guild)
-        channel = data_mem[guild_id] = Server(guild_id, channel_id, Mode.value_of(mode))
+        channel_id, mode, mention_everyone = get_channel_info_or_default(member.guild)
+        channel = data_mem[guild_id] = Server(guild_id, channel_id, Mode.value_of(mode), mention_everyone)
     
     alert_channel = client.get_channel(int(channel.notification_channel))
+
+    msg = ''
+    if channel.mention_everyone:
+        msg += '@everyone '
 
     if before.channel != after.channel:
         now = datetime.utcnow() + timedelta(hours=9)
         if before.channel is None:
             mode = channel.mode
             if mode == Mode.all or len(list(filter(lambda m: not m.bot, after.channel.members))) == 1:
-                msg = f'{now:%m/%d-%H:%M} に[{member.name}]さんがチャンネル[{after.channel.name}]で通話を始めました。'
+                msg += f'{now:%m/%d-%H:%M} に[{member.name}]さんがチャンネル[{after.channel.name}]で通話を始めました。'
                 await alert_channel.send(msg)
         # elif after.channel is None: 
         #     if len(list(filter(lambda m: not m.bot, before.channel.members))) == 0:
@@ -114,7 +135,7 @@ async def on_voice_state_update(member, before, after):
 
     if not before.self_stream and after.self_stream:
         now = datetime.utcnow() + timedelta(hours=9)
-        msg = f'{now:%m/%d-%H:%M} に[{member.name}]さんがチャンネル[{after.channel.name}]でライブ配信を始めました。'
+        msg += f'{now:%m/%d-%H:%M} に[{member.name}]さんがチャンネル[{after.channel.name}]でライブ配信を始めました。'
         await alert_channel.send(msg)
 
 client.run(token)
