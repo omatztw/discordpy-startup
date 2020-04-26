2080ae842911be5de7a1711eda17f8d3f228a489
おまつ先生 <33661309+omatztw@users.noreply.github.com>
2020-04-26 14:03:20 +0900

    Support Rose and Moen server raid notification

--- discordbot.py
+++ discordbot.py
@@ -5,13 +5,18 @@ import psycopg2
 import requests
 from psycopg2.extras import DictCursor
 from datetime import datetime, timedelta
-from server_info import Server, Mode
+from server_info import Server, Mode, ServerType
 
 
 client = commands.Bot(command_prefix="!")
 token = os.environ['DISCORD_BOT_TOKEN']
 database_url = os.environ.get('DATABASE_URL')
 sheet_url = os.environ.get('SHEET_URL')
+sheet_url_dict = {
+    'elph': os.environ.get('SHEET_URL_ELPH'),
+    'rose': os.environ.get('SHEET_URL_ROSE'),
+    'moen': os.environ.get('SHEET_URL_MOEN')
+}
 data_mem = dict()
 
 def get_connection():
@@ -20,7 +25,7 @@ def get_connection():
 def get_value_by_server_id(server_id):
     with get_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT voice_notification_channel as ch_id, mode as mode, mention_everyone as mention_everyone, notify as notify FROM server_info WHERE server_id='%s'" % (server_id,))
+            cur.execute("SELECT voice_notification_channel as ch_id, mode as mode, mention_everyone as mention_everyone, notify as notify, server_type as server_type FROM server_info WHERE server_id='%s'" % (server_id,))
             result = cur.fetchone()
             if result == None:
                 return None
@@ -29,7 +34,7 @@ def get_value_by_server_id(server_id):
 def get_all_data():
     with get_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT server_id as server_id, voice_notification_channel as ch_id, mode as mode, mention_everyone as mention_everyone, notify as notify FROM server_info")
+            cur.execute("SELECT server_id as server_id, voice_notification_channel as ch_id, mode as mode, mention_everyone as mention_everyone, notify as notify, server_type as server_type FROM server_info")
             result = cur.fetchall()
             return result
 
@@ -42,7 +47,13 @@ def upsert_channel_id(server_id, channel_id):
 def change_mode(server_id, mode):
     with get_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("UPDATE server_info SET mode = '%s' WHERE server_id = '%s'" % (mode, server_id))
+            cur.execute("UPDATE server_info SET mode = '%s' WHERE server_id = '%s'" % (mode.name, server_id))
+        conn.commit()
+
+def change_server(server_id, server_type):
+    with get_connection() as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute("UPDATE server_info SET server_type = '%s' WHERE server_id = '%s'" % (server_type.name, server_id))
         conn.commit()
 
 def update_mention_everyone(server_id, mention_everyone):
@@ -64,15 +75,15 @@ def get_channel_info_or_default(guild):
         if guild.system_channel:
             channel_id = guild.system_channel.id
         upsert_channel_id(guild.id, channel_id)
-        return channel_id, 'first', False, False
-    return channel["ch_id"], channel["mode"], channel["mention_everyone"], channel["notify"]
+        return Server(guild.id, channel_id)
+    return Server(guild.id, channel["ch_id"], Mode.value_of(channel["mode"]), channel["mention_everyone"], channel["notify"], ServerType.value_of(channel["server_type"]))
 
 def str2bool(s):
      return s.lower() in ["true", "t", "yes", "1", "on"]
 
-def get_raid_time():
+def get_raid_time(server_type = 'elph'):
     headers = {"content-type": "application/json"}
-    response = requests.get(sheet_url, headers=headers).json()
+    response = requests.get(sheet_url_dict[server_type], headers=headers).json()
     ron_list =  [datetime.strptime(x, '%Y/%m/%d %H:%M:%S').strftime('%H:%M:%S') for x in response["ron"]]
     modafu_list =  [datetime.strptime(x, '%Y/%m/%d %H:%M:%S').strftime('%H:%M:%S') for x in response["modafu"]]
     msg = """```
@@ -96,14 +107,13 @@ async def on_ready():
     global data_mem
     all_data = get_all_data()
     if all_data != None:
-        for key,ch_id, mode, mention_everyone, notify in all_data:
-            data_mem[key] = Server(key, ch_id, Mode.value_of(mode), mention_everyone, notify)
+        for key,ch_id, mode, mention_everyone, notify, server_type in all_data:
+            data_mem[key] = Server(key, ch_id, Mode.value_of(mode), mention_everyone, notify, ServerType.value_of(server_type))
 
 @client.event
 async def on_guild_join(guild):
     global data_mem
-    ch_id, mode, mention_everyone, notify = get_channel_info_or_default(guild)
-    data_mem[str(guild.id)] = Server(guild.id, ch_id, Mode.value_of(mode), mention_everyone, notify)
+    data_mem[str(guild.id)] = get_channel_info_or_default(guild)
 
 @client.command()
 async def oma(ctx, *arg):
@@ -115,8 +125,18 @@ async def oma(ctx, *arg):
         data_mem[str(ctx.message.guild.id)].notification_channel = ctx.message.channel.id
         await ctx.send("通知するチャンネルを[%s]に変更しました。" % ctx.message.channel)
     
+    if arg[0] == 'set_server' or arg[0] == 'ss':
+        if len(arg) != 2 or (arg[1] not in [e.name for e in ServerType]):
+            await ctx.send("Usage: `!oma set_server %s`" %  [e.name for e in ServerType])
+            return
+        server_type = ServerType.value_of(arg[1])
+        change_server(ctx.message.guild.id, server_type)
+        data_mem[str(ctx.message.guild.id)].server_type = server_type
+        await ctx.send("サーバーを[%s]に変更しました。" % server_type.value)
+
+    
     if arg[0] == 'raid':
-        msg = get_raid_time()
+        msg = get_raid_time(data_mem[str(ctx.message.guild.id)].server_type.name)
         await ctx.send(msg)
     
     if arg[0] == 'mode':
@@ -124,7 +144,7 @@ async def oma(ctx, *arg):
             await ctx.send("Usage: `!oma mode %s`" %  [e.name for e in Mode])
             return
         mode = Mode.value_of(arg[1])
-        change_mode(ctx.message.guild.id, mode.name)
+        change_mode(ctx.message.guild.id, mode)
         data_mem[str(ctx.message.guild.id)].mode = mode
         await ctx.send("通知モードを[%s]に変更しました。" % mode.value)
 
@@ -155,8 +175,7 @@ async def on_voice_state_update(member, before, after):
     guild_id = str(member.guild.id)
     channel = data_mem.get(guild_id)
     if channel == None:
-        channel_id, mode, mention_everyone, notify = get_channel_info_or_default(member.guild)
-        channel = data_mem[guild_id] = Server(guild_id, channel_id, Mode.value_of(mode), mention_everyone, notify)
+        channel = data_mem[guild_id] = get_channel_info_or_default(member.guild)
     
     if not channel.notify:
         return
--- server_info.py
+++ server_info.py
@@ -11,14 +11,27 @@ class Mode(Enum):
         return e
     raise ValueError("%sは有効なモードではありません" % target_value)
 
+class ServerType(Enum):
+  elph = 'エルフィンタ'
+  rose = 'ローゼンバーグ'
+  moen = 'モエン'
+
+  @classmethod
+  def value_of(cls, target_value):
+    for e in Mode:
+      if e.name == target_value:
+        return e
+    raise ValueError("%sは有効なサーバーではありません" % target_value)
+
 class Server(object):
   
-  def __init__(self, guild_id, notification_channel, mode = Mode.first, mention_everyone = False, notify = False):
+  def __init__(self, guild_id, notification_channel, mode = Mode.first, mention_everyone = False, notify = False, server_type = ServerType.elph):
     self.guild_id = guild_id
     self.notification_channel = notification_channel
     self.mode = mode
     self.mention_everyone = mention_everyone
     self.notify = notify
+    self.server_type = server_type
   
   
 
